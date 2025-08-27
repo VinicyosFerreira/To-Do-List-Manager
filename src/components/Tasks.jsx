241417a4fbf4a5af2ab27fd9ee7dd89ee0912f01
Vinicyos Ferreira <vinifr987@gmail.com>
2025-08-26 22:16:21 -0300

    feat: add task separator

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,10 +1,14 @@
 import Button from './Button';
 import AddIcon from '../assets/icons/addIcon.svg?react';
 import TrashIcon from '../assets/icons/trashIcon.svg?react';
+import SunIcon from '../assets/icons/sun.svg?react';
+import CloudSunIcon from '../assets/icons/cloud-sun.svg?react';
+import MoonIcon from '../assets/icons/moon.svg?react';
+import TasksSeparator from './TasksSeparator';
 
 const Tasks = () => {
    return (
-      <div className="flex w-full px-8 py-14">
+      <div className="w-full px-8 py-16">
          <div className="flex w-full justify-between">
             <div>
                <span className="text-xs font-semibold text-[#00ADB5]">
@@ -25,6 +29,24 @@ const Tasks = () => {
                   Nova Tarefa
                   <AddIcon />
                </Button>
+            </div>
+         </div>
+
+         {/* <TaskList /> */}
+         <div className="rounded-xl bg-white p-6">
+            {/* MANHÃ */}
+            <div className="space-y-3">
+               <TasksSeparator title="Manhã" icon={<SunIcon />} />
+            </div>
+
+            {/* TARDE */}
+            <div className="my-6 space-y-3">
+               <TasksSeparator title="Tarde" icon={<CloudSunIcon />} />
+            </div>
+
+            {/* NOITE */}
+            <div className="space-y-3">
+               <TasksSeparator title="Noite" icon={<MoonIcon />} />
             </div>
          </div>
       </div>
