9c7fbc7f7c66e082008e7688b8e809da2ec12016
Vinicyos Ferreira <vinifr987@gmail.com>
2025-08-28 15:03:47 -0300

    feat: ensure toast is shown after task update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import Sidebar from './components/Sidebar';
 import Tasks from './components/Tasks';
+import { Toaster } from 'sonner';
 function App() {
    return (
       <div className="flex">
+         <Toaster
+            toastOptions={{
+               style: {
+                  color: '#002C2E',
+               },
+            }}
+         />
          <Sidebar />
          <Tasks />
       </div>
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -8,6 +8,7 @@ import MoonIcon from '../assets/icons/moon.svg?react';
 import TasksSeparator from './TasksSeparator';
 import TASKS from '../constant/tasks';
 import TaskItem from './TaskItem';
+import { toast } from 'sonner';
 
 const Tasks = () => {
    const [tasks, setTasks] = useState(TASKS);
@@ -23,14 +24,17 @@ const Tasks = () => {
          }
 
          if (task.status === 'not_started') {
+            toast.success('Tarefa iniciada com sucesso!');
             return { ...task, status: 'in_progress' };
          }
 
          if (task.status === 'in_progress') {
+            toast.success('Tarefa concluida com sucesso!');
             return { ...task, status: 'done' };
          }
 
          if (task.status === 'done') {
+            toast.success('Tarefa reiniciada com sucesso!');
             return { ...task, status: 'not_started' };
          }
 
@@ -42,6 +46,7 @@ const Tasks = () => {
    const handleTaskDeleteClick = (taskId) => {
       const newTasks = tasks.filter((task) => task.id !== taskId);
       setTasks(newTasks);
+      toast.success('Tarefa excluida com sucesso!');
    };
 
    return (
