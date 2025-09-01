77b1896dd3fe12285fd138fc155e3636e9298f17
Vinicyos Ferreira <vinifr987@gmail.com>
2025-09-01 10:57:02 -0300

    feat: add size and secondary variant to Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,10 @@
-function Button({ children, variant, ...rest }) {
+function Button({
+   children,
+   variant = 'primary',
+   size = 'small',
+   className,
+   ...rest
+}) {
    const getVariantClass = () => {
       if (variant === 'primary') {
          return 'bg-[#00ADB5] text-white';
@@ -6,11 +12,24 @@ function Button({ children, variant, ...rest }) {
       if (variant === 'ghost') {
          return 'bg-transparent text-[#818181]';
       }
+
+      if (variant === 'secondary') {
+         return 'bg-[#EEEEEE] text-[#35383E]';
+      }
+   };
+
+   const getSizeClass = () => {
+      if (size === 'small') {
+         return 'text-xs py-1';
+      }
+      if (size === 'large') {
+         return 'text-sm py-2';
+      }
    };
 
    return (
       <button
-         className={`flex gap-2 rounded-md bg-[#00ADB5] px-3 py-1 text-xs font-semibold ${getVariantClass()} transition hover:opacity-75`}
+         className={`flex justify-center gap-2 rounded-md bg-[#00ADB5] px-3 text-xs font-semibold ${getVariantClass()} ${getSizeClass()} transition hover:opacity-75 ${className}`}
          {...rest}
       >
          {children}
