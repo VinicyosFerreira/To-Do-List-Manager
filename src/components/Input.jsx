9f7682894e26793b77e5efc2f4e507d2b09dc66d
Vinicyos Ferreira <vinifr987@gmail.com>
2025-09-01 10:58:47 -0300

    feat : add input component

---
+++ src/components/Input.jsx
@@ -0,0 +1,13 @@
+const Input = ({ label, ...rest }) => {
+   return (
+      <div className="flex flex-col space-y-1 text-left text-[#35383E]">
+         <label htmlFor={rest.id}>{label}</label>
+         <input
+            className="placeholder:[#9A9C9F] rounded-lg border border-solid border-[#ECECEC] px-4 py-3 outline-[#00ADB5] placeholder:text-sm"
+            {...rest}
+         />
+      </div>
+   );
+};
+
+export default Input;
